726e538fa5f461f49b96e7d5d9fc8c47a74e554f
Ze8c <Ze8c.Max@gmail.com>
2019-06-24 16:21:43 +0300

    #init

---
+++ ZRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZRepo
+//
+//  Created by Мак on 24/06/2019.
+//  Copyright © 2019 Мак. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ZRepoTests/ZRepoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZRepoTests.swift
+//  ZRepoTests
+//
+//  Created by Мак on 24/06/2019.
+//  Copyright © 2019 Мак. All rights reserved.
+//
+
+import XCTest
+@testable import ZRepo
+
+class ZRepoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
